ffec50f592dcab13526cecc16c063f6b3c961ac2
rodneykinney <rodneyk@allenai.org>
2015-09-04 17:18:08 -0700

    FunctionConverter wip

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,5 +9,6 @@ object Dependencies extends CoreDependencies {
   val scalaReflection = "org.scala-lang" % "scala-reflect" % "2.11.5"
   val commonsIO = "commons-io" % "commons-io" % "2.4"
   val parserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+  val reflectasm = "com.esotericsoftware" % "reflectasm" % "1.11.0"
   override val allenAiCommon = "org.allenai.common" %% "common-core" % "1.0.1"
 }
