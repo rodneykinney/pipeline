5374d07dab2e5848bde7103daf4e00fa42d84aef
rodneykinney <rodneyk@allenai.org>
2015-09-09 13:46:16 -0700

    TestPipeFunctions

--- src/main/scala/org/allenai/pipeline/PipeFunctions.scala
+++ src/main/scala/org/allenai/pipeline/PipeFunctions.scala
@@ -1,30 +1,28 @@
 package org.allenai.pipeline
 
 object PipeFunctions {
-  implicit class FunctionToProducer[T](p: Producer[T]) {
-    def pipeTo[O](func: T => O): Producer[O] = {
-      val funcInfo = new ClosureAnalyzer(func)
-      funcInfo.checkNoBadReferences
-      new Producer[O] {
-        override def create = func(p.get)
 
-        override def stepInfo =
-          PipelineStepInfo(func.getClass.getName)
-        .addParameters(funcInfo.parameters.toList: _*)
-      }
-    }
+  import scala.language.implicitConversions
+
+  implicit def F0toP[T](func: () => T): ProducerBuilder[T] = {
+    new ProducerBuilder0(func)
   }
+}
 
-  implicit def FtoP[T](func: () => T): Producer[T] = {
-    val funcInfo = new ClosureAnalyzer(func)
-    funcInfo.checkNoBadReferences
-    new Producer[T] {
-      def create = func()
-
-      def stepInfo =
-        PipelineStepInfo(func.getClass.getName)
-      .addParameters(funcInfo.parameters.toList: _*)
-    }
+trait ProducerBuilder[T] extends Producer[T] {
+  protected[this] var _stepInfo: PipelineStepInfo = PipelineStepInfo("")
+  def pipeTo[O2](func: T => O2) = new ProducerBuilder1(this, func)
+  def withName(name: String) = {
+    _stepInfo = _stepInfo.copy(className = name)
+    this
   }
+  def stepInfo = _stepInfo
+}
 
+class ProducerBuilder0[T](func: () => T) extends ProducerBuilder[T] {
+  def create = func()
+}
+
+class ProducerBuilder1[I, O](input: Producer[I], func: I => O) extends ProducerBuilder[O] {
+  def create = func(input.get)
 }
--- src/test/scala/org/allenai/pipeline/TestPipeFunctions.scala
+++ src/test/scala/org/allenai/pipeline/TestPipeFunctions.scala
@@ -1,26 +1,34 @@
 package org.allenai.pipeline
 
+import java.io.File
+
 import org.allenai.common.testkit.UnitSpec
 
 import scala.util.Random
 
 class TestPipeFunctions extends UnitSpec {
+  import IoHelpers._
   import PipeFunctions._
 
-  def foo() = {
-    val seed = 55
-    def rng() = {
-      val rand = new Random(seed)
-      (0 to 10).map(i => rand.nextDouble())
-    }
-    val closure1 = () => 1
-    val input: Producer[Iterable[Double]] = rng _
-
-    def exp(d: Iterable[Double]) = d.map(math.exp)
-    val pp = input.pipeTo(exp)
-
-    input.stepInfo.parameters should equal(Map("seed" -> seed.toString))
+  val seed = 55
+  val rng = () => {
+    val rand = new Random(seed)
+    (0 to 10).map(i => rand.nextDouble())
   }
-  foo()
 
+  "PipeFunctions" should "convert no-arg functions" in {
+    val pipeline = Pipeline(new File("pipeline-output"))
+    pipeline.Persist.Collection.asText[Double](rng.withName("Initial list"))
+  }
+
+  it should "convert single-arg functions" in {
+    val pipeline = Pipeline(new File("pipeline-output"))
+    def expOfList(d: Iterable[Double]) = d.map(math.exp)
+    val exp = rng.pipeTo(expOfList)
+
+    def logOfList(d: Iterable[Double]) = d.map(math.log)
+    val log = exp.pipeTo(logOfList)
+
+    pipeline.Persist.Collection.asText[Double](log)
+  }
 }
